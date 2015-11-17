98668e123839c6b984dcf9cbcfd285981d29bbdb
Emil Gedda <emil.gedda@emilgedda.se>
2015-11-17 03:29:53 +0100

    Minor changes to settings-file

--- Settings.hs
+++ Settings.hs
@@ -6,6 +6,7 @@ import Control.Monad
 import Data.Maybe
 import Control.Applicative
 
-findSettings = liftM3 maybe defval fun (lookupEnv "XDG_CONFIG_HOME") -- Check if $XDG_CONFIG_HOME is defined
-            where defval = liftM (</> ".config" </> "kattis" </> "kattisrc") getHomeDirectory -- Default value, ~/.config/kattis/kattisrc
-                  fun = return $ (</> "kattis" </> "kattisrc") -- $XDG_CONFIG_HOME/kattis/kattisrc
+findSettings = liftM3 maybe defval fun (lookupEnv "XDG_CONFIG_HOME") -- Is $XDG_CONFIG_HOME defined?
+            where defval = liftM (</> ".config" </> ending) getHomeDirectory -- No: ~/.config/kattis/kattisrc
+                  fun = return (</> ending) -- Yes: $XDG_CONFIG_HOME/kattis/kattisrc
+                  ending = "kattis" </> "kattisrc"
