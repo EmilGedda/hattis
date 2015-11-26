b990edcc20f6a11ccf6704b3cb6748dcac59f155
Emil Gedda <emil.gedda@emilgedda.se>
2015-11-26 21:34:58 +0100

    Added a Auth type

--- src/Settings.hs
+++ src/Settings.hs
@@ -1,4 +1,4 @@
-module Settings (loginAuth, settingsLocation, settingsExist) where
+module Settings (Auth, loginAuth, settingsLocation, settingsExist) where
 import System.Environment
 import System.Directory
 import System.FilePath
@@ -9,18 +9,24 @@ import Control.Applicative
 import Control.Monad
 import Data.List
 import Data.Bool
+import Data.Ord
+import Data.Function
 import qualified Data.Ini as I
 import qualified Data.HashMap.Strict as M
 import qualified Data.Text as T
 
+data Auth = Auth {submissionurl :: T.Text, token :: T.Text, username :: T.Text} deriving(Show)
+
 -- Returns the [(key, value)] of all fields in the .ini in FilePath
-loginAuth :: FilePath -> IO [(T.Text, T.Text)] 
-loginAuth = liftM (settingsValid . concatMap M.toList . M.elems) . parseIni
+loginAuth :: FilePath -> IO Auth
+loginAuth = liftM (auth . sortBy (comparing fst) . settingsValid
+              . concatMap M.toList . M.elems) . parseIni
+              where auth [(_,a),(_,b),(_,c)] = Auth a b c 
  
 -- Verify that neededvalues is a subset of parsed settings
 settingsValid :: [(T.Text, a)] -> [(T.Text, a)]
-settingsValid = bool (throw MalformedSettings) id =<< subset neededvalues . map fst
-                  where subset = flip $ all . flip elem
+settingsValid = (bool (throw MalformedSettings) <*> (length neededvalues ==) . length) . filtered
+                  where filtered = filter $ flip elem neededvalues . fst
 
 -- Try parsing the specified config file, kattisrc
 parseIni = liftM (either (throw . MiscError) I.unIni) . I.readIniFile
