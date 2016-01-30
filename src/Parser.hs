6cd6a674f6acabf7c1f4969182ca60ed219efd77
Emil Gedda <emil.gedda@emilgedda.se>
2016-01-31 00:56:13 +0100

    Minor bugfix with newlines and now filters the comments.

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,21 +3,21 @@ import Text.ParserCombinators.Parsec
 import Control.Applicative hiding ((<|>), many)
 
 data Token
-    = Section String
-    | KeyVal String String
-    | Comment String
+    = Comment String
+    | KeyVal  String String
+    | Section String
     deriving Show
-
-group :: CharParser () Token 
-group = Section <$> between (char '[') (char ']') (many1 letter) <* newline
 
 comment :: CharParser () Token
 comment = char '#' *> (Comment <$> manyTill anyChar newline)
 
-keyval :: CharParser () Token
-keyval = KeyVal <$> many1 letter <* string ": " <*> manyTill anyChar newline 
+group :: CharParser () Token 
+group = Section <$> between (char '[') (char ']') (many1 letter)
 
-final = many $ sepBy1 (keyval <|> group <|> comment) newline
+keyval :: CharParser () Token
+keyval = KeyVal <$> many1 letter <* string ": " <*> manyTill anyChar newline  
+
+final = many $ many comment *> many newline *> choice [keyval, group]
 
 tokenize :: String -> Either ParseError [Token]
-tokenize file = concat <$> parse final "kattisrc" file 
+tokenize = parse final "kattisrc"
