ef3c33d160395700676522a28081a2e634c43e34
Emil Gedda <emil.gedda@emilgedda.se>
2016-03-23 01:11:25 +0100

    Parser verifies and provides relevant errormessages

--- src/Settings/Lexer.hs
+++ src/Settings/Lexer.hs
@@ -1,9 +1,7 @@
-module Lexer (lexrc) where
+module Settings.Lexer (tokenize, Token(..)) where
 import Text.ParserCombinators.Parsec
+import Error
 import Control.Applicative hiding ((<|>), many, optional)
-import Control.Arrow
-import qualified Data.Map.Strict as M
-import qualified Data.List as L
 
 data Token
     = TComment String
@@ -25,18 +23,7 @@ eol = (comment *> return ()) <|> (optional (char '\r') *> newline *> return ())
 
 final = many $ many eol *> (keyval <|> group)
 
-tokenize :: FilePath -> Either ParseError [Token]
-tokenize x = (:) (TSection "general") <$> parse final "kattisrc" x
-
-structure :: [Token] -> M.Map String (M.Map String String)
-structure = M.fromList . map (second M.fromList . prepareMap) . L.groupBy issec  
-
-tuple (TKeyVal a b) = (a,b)
-prepareMap ((TSection s):xs) = (s,map tuple xs)
-
-
-issec (TSection _) (TKeyVal _ _) = True
-issec _ _ = False
-
-lexrc :: FilePath -> Either ParseError (M.Map String (M.Map String String))
-lexrc x = structure <$> tokenize x 
+tokenize :: FilePath -> Either KattisError [Token]
+tokenize x = case (:) (TSection "general") <$> parse final "kattisrc" x of
+                Left x ->  Left . ParseFail $ show x
+                Right y -> Right y
--- src/Settings/Parser.hs
+++ src/Settings/Parser.hs
@@ -1,39 +1,67 @@
-module Parser where
-import Lexer
+module Settings.Parser (Setting(..), SettingsStorage) where
+import Control.Arrow
+import Control.Monad
 import Data.Char
-import Data.List
+import Data.Maybe
+import Error
+import Settings.Lexer
 import System.Directory
+import Text.ParserCombinators.Parsec
+import qualified Data.List as L
+import qualified Data.Map.Strict as M
 
-type Schema = [Spec (String -> Bool)]
-type Settings = [Spec String]
+type Schema = [(Setting, String -> Bool)]
+type SettingsStorage = M.Map String (M.Map String String)
 
-data Spec a 
-    = Section String [Spec a]
-    | KeyVal String a 
+data Setting
+    = Username
+    | Token         
+    | LoginUrl     
+    | SubmissionUrl
+    deriving Show
 
-instance Eq (Spec x) where 
-        (==) (Section a _) (Section b _) = a == b
-        (==) (KeyVal a _)  (KeyVal b _)  = a == b
+class KeyVal a where
+    key :: a -> String
+    val :: a -> String
 
-instance Ord (Spec x) where
-        compare (Section a _) (Section b _) = compare a b
-        compare (KeyVal  a _) (KeyVal  b _) = compare a b
+instance KeyVal Setting where
+    key Username      = "user"
+    key Token         = "user"
+    key LoginUrl      = "kattis"
+    key SubmissionUrl = "kattis"
 
-instance Show (Spec x) where
-        show (Section a b) = a ++ ": [" ++ intercalate "," (map show b) ++ "]"
-        show (KeyVal a _)  = a
+    val Username      = "username"
+    val Token         = "token"
+    val LoginUrl      = "loginurl"
+    val SubmissionUrl = "submissionurl"
+
 
 valid :: Schema
 valid = let f fun = all (==True) . map fun in
-        [Section "user" [
-                KeyVal "username" (f isAlpha),      -- TODO: Research
-                KeyVal "token"    (f isHexDigit)
-            ],
-         Section "kattis" [
-                KeyVal "loginurl"      (f isAscii), -- TODO: Fix URL
-                KeyVal "submissionurl" (f isAscii)
-            ]
-        ]
+        [(Username, f isAlpha), (Token, f isHexDigit),
+         (LoginUrl, f isAscii), (SubmissionUrl, f isAscii)]
 
-check (KeyVal a f) (KeyVal b c) = a == b && f c
-check (Section a x) (Section b y) = a == b && all (==True) (zipWith check x y)
+verify :: SettingsStorage -> Schema -> Either KattisError SettingsStorage
+verify sett = g . map (\(s,_) -> ErroneousSettings (key s) (val s))
+                     . filter (not . snd) . map apply 
+        where apply (a,b) = (a, f $ b <$> getKey sett a)
+              f = fromMaybe False
+              g [] = Right sett
+              g (x:_) = Left x
+
+structure :: [Token] -> SettingsStorage 
+structure = M.fromList . map (second M.fromList . prepareMap) . L.groupBy issec  
+        where issec (TSection _) (TKeyVal _ _) = True
+              issec _ _ = False
+
+prepareMap (TSection s:xs) = (s,map tuple xs)
+        where tuple (TKeyVal a b) = (a,b)
+
+-- TODO: Move to re-exporting module
+lexrc :: String -> Either KattisError SettingsStorage
+lexrc x = structure <$> tokenize x 
+
+--test = ((`verify` valid) =<<) . lexrc <$> readFile "kattisrc"
+
+getKey :: SettingsStorage -> Setting -> Maybe String
+getKey a b = (val b `M.lookup`) =<< (key b `M.lookup` a)
